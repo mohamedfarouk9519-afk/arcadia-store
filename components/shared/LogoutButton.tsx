bbb90ac65cb6c695b009a962c670423e70f6e1bc
mohamed <mohamedfarouk9519@gmail.com>
2026-03-30 09:09:52 -0700

    show logout button

--- components/shared/LogoutButton.tsx
+++ components/shared/LogoutButton.tsx
@@ -17,7 +17,7 @@ export default function LogoutButton() {
   return (
     <button
       onClick={handleLogout}
-      className="rounded-xl bg-red-600 px-4 py-2 text-white"
+      className="rounded-xl bg-red-500 px-4 py-2 text-white font-bold"
     >
       تسجيل الخروج
     </button>
