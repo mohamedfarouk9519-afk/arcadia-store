25d2b2b53fb9967290be1f8991e3c991c680b46f
mohamed <mohamedfarouk9519@gmail.com>
2026-03-30 14:59:58 -0700

    fix cookies async

--- lib/api-auth.ts
+++ lib/api-auth.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
 export async function isAdminAuthenticated() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const isAdmin = cookieStore.get("admin_auth")?.value === "true";
   return isAdmin;
 }
